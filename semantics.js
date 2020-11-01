b5c18697762398a627360149ee73ae8483d7bc97
Paul Tarvydas <guitarvydas@gmail.com>
2020-11-01 17:22:01 -0500

    wip: break

--- semantics.js
+++ semantics.js
@@ -6,10 +6,10 @@ semantics.addOperation(
 	MatcherStatement: function (statements) { return statements.transpile ().join('\n'); },
 	Statement: function (statement, _period) { return statement.transpile (); },
 	
-	ClearStatement: function (_clear) { return "clear();\n";},
-	Rule: function (head, _eq, body) { return head.transpile () + " " + body.transpile ();},
-	Fact: function (head) { return head.transpile (); },
-	Query: function (_match, _lpar, expression, _rpar) { return expression.transpile ();},
+	ClearStatement: function (_clear) { return "clear();";},
+	Rule: function (head, _eq, body) { return `rule (${head.transpile ()}, ${body.transpile ()});`;},
+	Fact: function (head) { return `fact (${head.transpile ()});` },
+	Query: function (_match, _lpar, expression, _rpar) { return `query(${expression.transpile ()});`;},
 
 	BinaryRelation: function (relation, _lpar, subject, _comma, object, _rpar) { return relation.transpile () + subject.transpile () + object.transpile ();},
 	UnaryRelation: function (relation, _lpar, subject, _rpar) { return relation.transpile () + " " + subject.transpile ();},
@@ -26,16 +26,18 @@ semantics.addOperation(
 	Body: function (expression) { return expression.transpile ();},
 
 	Formal: function (f) { return f.transpile (); },
-	MatchExpression: function (factor, _or, orFactors) { return factor.transpile () + " " + orFactors.transpile ().join ("|");},
-	MatchFactor: function (atom, _and, andAtoms) { return atom.transpile () + andAtoms.transpile ().join ("&");},
+	MatchExpression: function (factor, _or, orFactors) { return factor.transpile () + "|" + orFactors.transpile ().join ("|");},
+	MatchFactor: function (atom, _and, andAtoms) { return atom.transpile () + "&" + andAtoms.transpile ().join ("&");},
 	MatchAtom: function (atom) { return atom.transpile (); },
 	Keyword: function (k) { return k.transpile (); },
 	BinaryFunctor: function (id, _lpar, primary1, _comma, primary2, _rpar) { return  id.transpile() + " " + primary1.transpile () + " " + primary2.transpile ();},
 	UnaryFunctor: function (id, _lpar, primary, _rpar) { return id.transpile() + " " + primary.transpile ();},
 	NonaryFunctor: function (id) { return id.transpile (); },
 	Primary: function (primary) { return primary.transpile (); },
-	identifier: function (lc, cs) { return lc.transpile () + cs.transpile ().join(''); },
-	logicVariable: function (uc, cs) { return uc.transpile () + cs.transpile ().join(''); },
+	identifier: function (lc, cs) { return `"${lc.transpile () + cs.transpile ().join('')}"`; },
+	logicVariable: function (uc, cs) { 
+	    return `lVar(${uc.transpile () + cs.transpile ().join('')})`; 
+	},
 	lowerCaseLetter: function (c) { return c.transpile (); },
 	upperCaseLetter: function (c) { return c.transpile (); },
 	identLetter: function (c) { return c.transpile (); },
@@ -43,5 +45,6 @@ semantics.addOperation(
 	_terminal: function () { return this.primitiveValue; }
     });
 
-console.log (semantics (parseTree).transpile ());
-
+if (parseTree.succeeded ()) {
+    console.log (semantics (parseTree).transpile ());
+};
